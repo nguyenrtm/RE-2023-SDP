8202d27ae77efd3f9e2b3284c7cd876c88d306d9
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2023-12-03 00:19:15 +0700

    new dep

--- src/custom_dataset.py
+++ src/custom_dataset.py
@@ -16,7 +16,7 @@ class CustomDataset(Dataset):
             else:
                 i += 1
 
-    def batch_padding(self, batch_size):
+    def batch_padding(self, batch_size, min_batch_size=3):
         current = 0
         to_return = []
 
@@ -32,7 +32,7 @@ class CustomDataset(Dataset):
             current += batch_size
 
         batch = self.data[current:]
-        max_len_in_batch = max(max([x.shape[0] for x in batch]), 3)
+        max_len_in_batch = max(max([x.shape[0] for x in batch]), min_batch_size)
 
         for i in range(len(batch)):
             tmp = F.pad(batch[i], (0, 0, 0, max_len_in_batch - batch[i].shape[0]), "constant", 0)
@@ -42,7 +42,7 @@ class CustomDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, idx):
             sample = self.data[idx]
             label = self.labels[idx]
